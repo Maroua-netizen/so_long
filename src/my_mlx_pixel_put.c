9c40a30b160726fe125b4d59abe1c63a797f3749
Maroua Mounsif <mmounsif@c1r2p5.1337.ma>
2025-02-01 17:53:33 +0100

    Refactor code for improved readability and maintainability

--- src/my_mlx_pixel_put.c
+++ src/my_mlx_pixel_put.c
@@ -6,7 +6,7 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 17:21:10 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/01 17:47:32 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/01 17:53:15 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
 	char	*dst;
 
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
