02f7b55bb05ecfcd12797ded0ab7012b8770db17
Maroua Mounsif <mmounsif@c1r2p5.1337.ma>
2025-02-01 13:31:11 +0100

    Update image handling in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:25:31 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/01 12:42:34 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:51:54 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ int main()
 {
 	void	*mlx_ptr;
 	void	*mlx_win;
-	void	*img;
+	t_data	img;
 	
 	mlx_ptr = mlx_init();
 	mlx_win = mlx_new_window(mlx_ptr, 640, 360, "Flower Lover <3");
-	img = mlx_new_image(mlx_ptr, 640, 360);
+	img.img_ptr = mlx_new_image(mlx_ptr, 640, 360);
+	img.addr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, &img.line_length, &img.endian);
 	mlx_loop(mlx_ptr);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:21:25 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/01 12:43:04 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:49:28 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 # define SO_LONG_H
 
 # include <mlx.h>
+
+typedef struct	s_data {
+	void	*img_ptr;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 #endif
