17329000476a4043921f71565a7b0142ea3d86df
Maroua Mounsif <mmounsif@c1r3p9.1337.ma>
2025-02-08 19:52:51 +0100

    Refactor map and image in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:25:31 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/08 16:26:45 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/08 19:47:32 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,12 @@ int	main(void)
 	mlx_ptr = mlx_init();
 	// print_tab(get_map("map.ber"));
 	check_map("map.ber");
-	// img.img_ptr = mlx_new_image(mlx_ptr, 640, 352);
-	//tiles are 32x32 pixels so with the resolution above, we will have 20 tiles horizontally and 11 vertically.
+	// img.img_ptr = mlx_new_image(mlx_ptr, 704, 448);
+	//tiles are 64x64 pixels so with the resolution above, we will have 11 tiles horizontally and 7 vertically.
 	// img.addr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, 
 	// 		&img.line_length, &img.endian);
 	// my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	img = mlx_xpm_file_to_image(mlx_ptr, "textures/Background.xpm", &img_width, &img_height);
+	img = mlx_xpm_file_to_image(mlx_ptr, "textures/Grass.xpm", &img_width, &img_height);
 	mlx_win = mlx_new_window(mlx_ptr, img_width, img_height, "Flower Lover <3");
 	mlx_put_image_to_window(mlx_ptr, mlx_win, img, 0, 0);
 	mlx_loop(mlx_ptr);
