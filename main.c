0fb5c0f66197ab521b2ced0356ed9befd118dd61
Maroua Mounsif <mmounsif@c1r2p7.1337.ma>
2025-02-09 18:35:34 +0100

    Refactor window size calculation in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:25:31 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/09 18:30:13 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/09 18:34:12 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	main(int ac, char **av)
 	void	*mlx_ptr;
 	char	**map;
 	void	*mlx_win;
-	int		win_height;
-	int		win_width;
 
 	if (ac == 2)
 	{
@@ -26,10 +24,9 @@ int	main(int ac, char **av)
 		{
 			mlx_ptr = mlx_init();
 			map = get_map(av[1]);
-			win_height = TILE_SIZE * map_line_count(av[1]);
-			win_width = TILE_SIZE * (ft_strlen(map[0]) - 1);
-			mlx_win = mlx_new_window(mlx_ptr, win_width, win_height,
-					"Flower Lover <3");
+			mlx_win = mlx_new_window(mlx_ptr, 
+					TILE_SIZE * (ft_strlen(map[0]) - 1), 
+					TILE_SIZE * map_line_count(av[1]), "Flower Lover <3");
 			display_background(map, mlx_ptr, mlx_win);
 			display_map(map, mlx_ptr, mlx_win);
 			free_map(map);
