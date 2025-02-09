ae5bb77306cc3eb1e930939ee363d5ec6ff4cd4b
Maroua Mounsif <mmounsif@c1r2p7.1337.ma>
2025-02-09 17:25:32 +0100

    Refactor main function to accept command line arguments

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: mmounsif <mmounsif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:25:31 by mmounsif          #+#    #+#             */
-/*   Updated: 2025/02/09 13:51:57 by mmounsif         ###   ########.fr       */
+/*   Updated: 2025/02/09 17:23:25 by mmounsif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	void	*mlx_ptr;
 	char	**map;
@@ -20,17 +20,22 @@ int	main(void)
 	int		win_height;
 	int		win_width;
 
-	if (check_map("map.ber"))
+	if (ac == 2)
 	{
-		mlx_ptr = mlx_init();
-		map = get_map("map.ber");
-		win_height = 64 * map_line_count("map.ber");
-		win_width = 64 * (ft_strlen(map[0]) - 1);
-		mlx_win = mlx_new_window(mlx_ptr, win_width, win_height,
-				"Flower Lover <3");
-		display_background(map, mlx_ptr, mlx_win);
-		display_map(map, mlx_ptr, mlx_win);
-		free_map(map);
-		mlx_loop(mlx_ptr);
+		if (check_map(av[1]))
+		{
+			mlx_ptr = mlx_init();
+			map = get_map(av[1]);
+			win_height = 64 * map_line_count(av[1]);
+			win_width = 64 * (ft_strlen(map[0]) - 1);
+			mlx_win = mlx_new_window(mlx_ptr, win_width, win_height,
+					"Flower Lover <3");
+			display_background(map, mlx_ptr, mlx_win);
+			display_map(map, mlx_ptr, mlx_win);
+			free_map(map);
+			mlx_loop(mlx_ptr);
+		}
 	}
+	else
+		ft_printf("Too many parameters!");
 }
